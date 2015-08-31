f07010b19f7e3d4091b68ba97cb71ea1c0e00ba0
Juergen Windhaber <juergen.windhaber@gmail.com>
2015-08-31 17:30:03 +0200

    added simple test

---
+++ src/test/groovy/org/springframework/basics/section01/SimpleApplicationContextTest.groovy
@@ -0,0 +1,18 @@
+package org.springframework.basics.section01
+
+import org.springframework.context.support.StaticApplicationContext
+import spock.lang.Specification
+
+class SimpleApplicationContextTest extends Specification {
+
+    def "test a simple application context"() {
+        expect:
+            def context = new StaticApplicationContext()
+            context.registerSingleton("serviceA", ServiceA)
+            context.getBean(ServiceA) != null
+            context.getBean("serviceA") != null
+    }
+
+    public static class ServiceA{}
+
+}
