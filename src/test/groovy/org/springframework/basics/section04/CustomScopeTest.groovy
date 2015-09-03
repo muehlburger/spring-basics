6796117fb7f76e5b68e81dc09ce2410a349a2015
cweber <Christian-Weber1@gmx.at>
2015-09-03 09:28:29 +0200

    section04 (custom scope) modified

--- src/test/groovy/org/springframework/basics/section04/CustomScopeTest.groovy
+++ src/test/groovy/org/springframework/basics/section04/CustomScopeTest.groovy
@@ -16,19 +16,34 @@ class CustomScopeTest extends Specification {
     @Autowired
     private ApplicationContext context
 
-    def "thead scope registration"() {
+    def "thread scope registration"() {
         expect:
+            def thread1Bean1 = null
+            def thread1Bean2 = null
+            def thread2Bean1 = null
+            def thread2Bean2 = null
+
             def thread1 = Thread.start {
-                println "${Thread.currentThread().id} ${context.getBean(BeanA.class)}"
-                println "${Thread.currentThread().id} ${context.getBean(BeanA.class)}"
+                thread1Bean1 = context.getBean(BeanA.class)
+                thread1Bean2 = context.getBean(BeanA.class)
+                println "${Thread.currentThread().id} $thread1Bean1"
+                println "${Thread.currentThread().id} $thread1Bean2"
             }
             def thread2 = Thread.start {
-                println "${Thread.currentThread().id} ${context.getBean(BeanA.class)}"
-                println "${Thread.currentThread().id} ${context.getBean(BeanA.class)}"
+                thread2Bean1 = context.getBean(BeanA.class)
+                thread2Bean2 = context.getBean(BeanA.class)
+                println "${Thread.currentThread().id} $thread2Bean1"
+                println "${Thread.currentThread().id} $thread2Bean2"
             }
 
             thread1.join()
             thread2.join()
+
+            thread1Bean1 == thread1Bean2
+            thread2Bean1 == thread2Bean2
+
+            thread1Bean1 != thread2Bean1
+            thread1Bean2 != thread2Bean2
     }
 
     @Configuration
